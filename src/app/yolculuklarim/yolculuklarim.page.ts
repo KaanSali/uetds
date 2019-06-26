ccc3bc818b2165fe5c979906cc097b59c9a9de8f
KaanSali <kaan_sali@hotmail.com>
2019-06-26 16:20:34 +0300

    Sirket Menusu ve yolculuklarım eklendi

---
+++ src/app/yolculuklarim/yolculuklarim.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yolculuklarim',
+  templateUrl: './yolculuklarim.page.html',
+  styleUrls: ['./yolculuklarim.page.scss'],
+})
+export class YolculuklarimPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
